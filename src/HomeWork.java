28aea45dddb6738790da13ad0fd28b4c71684e65
Hryhorii Tsiupak <Gorg.lvov@Gmail.com>
2022-08-30 17:17:21 +0300

    Initial commit

---
+++ src/HomeWork.java
@@ -0,0 +1,21 @@
+public class HomeWork {
+    public static void main (String[]arg){
+
+        String gfriend1 =  "Natalka";
+        String gfriend2 = "natalka";
+
+        if (gfriend1 .equals(gfriend2)) {
+            System.out.print("Так " + gfriend1);
+        }
+        else {
+            System.out.println("No");
+
+        }
+
+        }
+      }
+
+
+
+
+
